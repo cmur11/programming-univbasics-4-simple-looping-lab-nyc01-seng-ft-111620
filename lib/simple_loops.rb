7d6046052747be6010a60e5281186b4bc5b52893
Conor Murnane <conormurnane11@gmail.com>
2020-10-28 22:21:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,7 +12,7 @@ end
 
 def loop_message_n_times(string,integer)
 counter = 0
- while counter <= integer do  
+ while counter < integer do  
     puts string
     counter +=1
 end
@@ -21,7 +21,7 @@ end
 def output_array(array)
 counter = 0
 
-while counter <= array.length do
+while counter < array.length do
       puts array[counter]
       counter += 1
 end
@@ -30,7 +30,7 @@ end
  def return_string_array(array)
    counter = 0
    new_array = []
-  while counter <= array.length do
+  while counter < array.length do
     new_array << array[counter].to_s
     counter += 1
  end
